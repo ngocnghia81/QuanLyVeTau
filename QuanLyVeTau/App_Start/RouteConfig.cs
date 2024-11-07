326064fa872f9ebc6a657acdf6e2b7c8628ab1b3
ngocnghia81 <ngocnghia.workspace@gmail.com>
2024-11-07 15:54:18 +0700

    complete LayoutQuanTri

--- QuanLyVeTau/App_Start/RouteConfig.cs
+++ QuanLyVeTau/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace QuanLyVeTau
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Ve", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "QuanTri", action = "Index", id = UrlParameter.Optional }
             );
         }
     }

f2472b5bd20681183e7336cdd4903286ddbe4bed
ngocnghia81 <ngocnghia.workspace@gmail.com>
2024-11-07 15:37:44 +0700

    complete LayoutNguoiDung

--- QuanLyVeTau/App_Start/RouteConfig.cs
+++ QuanLyVeTau/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace QuanLyVeTau
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Ve", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
